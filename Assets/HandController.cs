5d4e6edd3eef94884b9cb779378a3268c3d6d8ce
Császár-Kozma Réka <rekuci86@gmail.com>
2020-08-03 12:25:18 +0200

    Project start commit

---
+++ Assets/HandController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+	//https://www.youtube.com/watch?v=ixM2W2tPn6c
+	public float mouseSensitivity;
+	public float speed;
+
+	public float penUpYposition;
+	public float penDownYPosition;
+
+	Rigidbody rb;
+	Vector3 startPosition;
+
+	Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		startPosition = this.transform.position;
+		rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float mouseXValue = Input.GetAxis("Mouse X");
+		float mouseYValue = Input.GetAxis("Mouse Y");
+
+
+		if (Input.GetMouseButton(1))
+		{
+			movement = new Vector3(mouseXValue, 0, mouseYValue) * mouseSensitivity;
+		}
+			
+		
+    }
+
+	void FixedUpdate()
+	{
+		if (movement != new Vector3(0, 0, 0))
+		{
+			rb.MovePosition(this.transform.position + movement * speed * Time.deltaTime);
+		}
+
+
+	}
+}
